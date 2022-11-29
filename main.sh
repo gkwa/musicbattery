a1839c2bb8ec1d5af7c44770065c31b1eb7283d8
Taylor Monacelli <taylormonacelli@gmail.com>
2022-11-29 14:37:25 -0800

    Move secrets to subdir

--- main.sh
+++ main.sh
@@ -2,35 +2,40 @@
 
 set -e
 set -x
+set -u
 
 base="$(pwd)"
 cd "$base"
 
-rm -f main.key
-rm -f mysecret2.yaml
-rm -f mysealedsecret2.yaml
+# start clean
+rm -rf "$base/secrets"
+kind delete cluster --name sealed1
+kind delete cluster --name sealed2
 
-kind delete cluster
+kind get clusters
+kind create cluster --wait 3m --name sealed1
+
+mkdir -p "$base/secrets"
 
 kubectl config get-contexts
 
 helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
 helm repo update sealed-secrets
 
-kind create cluster --wait 3m
 helm install sealed-secrets --namespace kube-system sealed-secrets/sealed-secrets
 
 kubectl wait --for=condition=ready pod -l app.kubernetes.io/instance=sealed-secrets --namespace kube-system
 
 # keep this public/private key OMG safe:
-kubectl get secret -n kube-system -l sealedsecrets.bitnami.com/sealed-secrets-key -o yaml >"$base/main.key"
-ls -la "$base/main.key"
+kubectl get secret -n kube-system -l sealedsecrets.bitnami.com/sealed-secrets-key -o yaml >"$base/secrets/main.key"
+ls -la "$base/secrets/main.key"
 
-echo -n 'PassW@rd1' | kubectl create secret generic mysecret --dry-run=client --from-file=mypassword=/dev/stdin -o yaml >"$base/mysecret2.yaml"
-kubeseal --controller-name=sealed-secrets -o yaml <"$base/mysecret2.yaml" >"$base/mysealedsecret2.yaml"
-ls -la "$base/mysecret2.yaml" "$base/mysealedsecret2.yaml"
+echo -n 'PassW@rd1' | kubectl create secret generic mysecret --dry-run=client \
+    --from-file=mypassword=/dev/stdin -o yaml >"$base/secrets/mysecret2.yaml"
+kubeseal --controller-name=sealed-secrets -o yaml <"$base/secrets/mysecret2.yaml" >"$base/secrets/mysealedsecret2.yaml"
+ls -la "$base/secrets/mysecret2.yaml" "$base/secrets/mysealedsecret2.yaml"
 
-kubectl create -f "$base/mysealedsecret2.yaml"
+kubectl create -f "$base/secrets/mysealedsecret2.yaml"
 kubectl get secret mysecret -o yaml -n default
 kubectl get secret mysecret -o jsonpath="{.data.mypassword}" | base64 --decode
 echo
@@ -39,24 +44,26 @@ kubectl get secrets --all-namespaces
 kubectl get secrets --all-namespaces | sed 1d | wc -l
 
 # invoke disaster
-kind delete cluster
+kind delete cluster --name sealed1
 
 # create new cluster
-kind create cluster --wait 3m
+kind create cluster --wait 3m --name sealed2
 helm install sealed-secrets --namespace kube-system sealed-secrets/sealed-secrets
 kubectl wait --for=condition=ready pod -l app.kubernetes.io/instance=sealed-secrets --namespace kube-system
 
 # re-add our private key and trigger controller to reload it
-kubectl apply -f main.key
+kubectl apply -f "$base/secrets/main.key"
 kubectl delete pods -l app.kubernetes.io/name=sealed-secrets -n kube-system
 
 kubectl get pods -l app.kubernetes.io/name=sealed-secrets -n kube-system
 kubectl wait --for=condition=ready pod -l app.kubernetes.io/instance=sealed-secrets --namespace kube-system
 
-kubectl apply -f mysealedsecret2.yaml
+kubectl apply -f "$base/secrets/mysealedsecret2.yaml"
 
 kubectl get secrets --all-namespaces
 kubectl get secrets --all-namespaces | sed 1d | wc -l
 
 kubectl get secret mysecret -o jsonpath="{.data.mypassword}" | base64 --decode
 echo
+
+kind delete cluster --name sealed2
